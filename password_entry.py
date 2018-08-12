d7e12ec36e63a4add8ef3120070b50a51cb59d6d
vickiedge <vicki.edge@my.jcu.edu.au>
2018-08-12 11:43:10 +0930

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Vicki Edge"""
+
+MIN_LENGTH = 5
+
+password = input("Enter password: ")
+if len(password) < MIN_LENGTH:
+    print("Invalid password")
+    password = input("Enter password: ")
+length = len(password)
+print("*" * (length))
